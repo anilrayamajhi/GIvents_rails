9f8cc3c29da3537c53578d385753c87c3cee2227
Anil Rayamajhi <anilrm07@gmail.com>
2016-09-18 17:33:21 -0700

    Event conroller, rake db:reset to reset the db from postgress

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+events = Event.create([{e_name: "rave", e_address:"3232 Robertson Blvd", e_date:'20161010', e_time:'002225', e_description:"jsnalnsjbjajspcijjas knscoancouos"}])
