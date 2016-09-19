63a0f76006c39c8355c7c495357ca9586e744023
Anil Rayamajhi <anilrm07@gmail.com>
2016-09-19 16:19:29 -0700

    User model

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
 
   root 'events#index'
   resources :events
+
+  #sessions routes
+  get '/logout' => 'sessions#destroy'
+  resources :sessions, only: [:new, :create]
+
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160919214005) do
+ActiveRecord::Schema.define(version: 20160919231052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 20160919214005) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.datetime "e_datetime"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
